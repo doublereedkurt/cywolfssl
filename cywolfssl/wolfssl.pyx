c8adaf5ebc5a669ad73f558fb93d84ebfbe5373f
Kurt Rose <=>
2015-12-03 16:55:33 -0800

    attempting first build

---
+++ cywolfssl/wolfssl.pyx
@@ -0,0 +1,5 @@
+
+cdef extern from "wolfssl/ssl.h" nogil:
+    void wolfSSL_Init()
+
+wolfSSL_Init()
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+import os, os.path
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+WOLFSSL_INCLUDE = os.path.join(DIR, "lib", "wolfssl-3.7.0", "wolfssl-3.7.0")
+
+extension = Extension('wolfssl', sources=['cywolfssl/wolfssl.pyx'],
+    include_dirs=[WOLFSSL_INCLUDE])
+
+setup(
+    name='cywolfssl',
+    ext_modules=cythonize(extension)
+    )
