e42eed221c36d6dfc00a72aeb9f5bbab4c50dfdd
Kurt Rose <kurt@kurtrose.com>
2015-12-03 17:29:02 -0800

    got wolfssl compiling statically on linux

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ DIR = os.path.dirname(os.path.abspath(__file__))
 WOLFSSL_INCLUDE = os.path.join(DIR, "lib", "wolfssl-3.7.0", "wolfssl-3.7.0")
 
 extension = Extension('wolfssl', sources=['cywolfssl/wolfssl.pyx'],
-    include_dirs=[WOLFSSL_INCLUDE])
+    include_dirs=[WOLFSSL_INCLUDE],
+    extra_objects=[DIR + "/lib/wolfssl-3.7.0/wolfssl-3.7.0/src/.libs/libwolfssl.a"])
 
 setup(
     name='cywolfssl',
